9cf079da5f9f9069fb317ca6fd6706efe9ce7d53
Victor Mulders <v.mulders@vgz.nl>
2024-09-20 14:06:26 +0200

    temp

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,16 +5,16 @@ import { Analytics } from "@vercel/analytics/react";
 import Lenis from '@studio-freight/lenis';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-    useEffect( () => {
-        const lenis = new Lenis()
-
-        function raf(time: number) {
-            lenis.raf(time)
-            requestAnimationFrame(raf)
-        }
-
-        requestAnimationFrame(raf)
-    },[])
+    // useEffect( () => {
+    //     const lenis = new Lenis()
+    //
+    //     function raf(time: number) {
+    //         lenis.raf(time)
+    //         requestAnimationFrame(raf)
+    //     }
+    //
+    //     requestAnimationFrame(raf)
+    // },[])
 
     return (
         <>
