630793d6617865aa7e81f06c33efd5c22d7eace5
Victor Mulders <v.mulders@vgz.nl>
2024-09-23 00:00:02 +0200

    test 3

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,9 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import Lenis from '@studio-freight/lenis';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-    useEffect( () => {
-        const lenis = new Lenis()
-
-        function raf(time: number) {
-            lenis.raf(time)
-            requestAnimationFrame(raf)
-        }
-
-        requestAnimationFrame(raf)
-    },[])
 
     return (
         <>
