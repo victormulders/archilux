501e6fbfa1a7d87931890d04392853aff3a7f4e3
Victor Mulders <v.mulders@vgz.nl>
2024-09-23 15:49:46 +0200

    Revert "test 1000"
    This reverts commit 4bb041f2c232813690a909cdf86f2e1504066e0f.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -255,7 +255,7 @@ export default function Home() {
             </Link>
           </div>
           <div className="w-full">
-            {/*<Faq />*/}
+            <Faq />
           </div>
         </section>
 
